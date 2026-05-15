cdf59182999f0c8edb7a9b13597c25c8ae865705
Arockia Rashmi R <25bcab13@kristujayanti.com>
2026-05-15 19:29:30 +0530

    Initial Commit: Creation of Project

---
+++ Project_Browser_LandingPage/scrips.js
@@ -0,0 +1,13 @@
+const taskInput = document.getElementById('taskInput')
+const toDo = document.getElementById('toDo__list')
+const toDoImp = document.getElementById('toDo_imp__list')
+
+taskInput.addEventListener('keydown', function(event))
+{
+    //check if user pressed enter
+    if(event.key === 'Enter')
+        const taskValue = taskInput.value;
+
+        //prevent empty tasks
+        
+}
